30746ca1f15a99f722c98e6378983b65cf0fd619
BrianXS <brian@rincon.dev>
2020-11-29 22:17:33 -0500

    The whole home section is almost finished, although some stuff is missing like user info and follow on hover

--- src/modules/root.module.ts
+++ src/modules/root.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { RootContainer } from '../containers/root.container/root.container';
 import { NavbarComponent } from "../components/navbar.component/navbar.component";
+import { HomeLeftSidebarComponent } from "../components/home.left.sidebar/home.left.sidebar.component"
+import { RecentPostComponent } from "../components/recent.post/recent.post.component";
+import { HomeRightSidebarComponent } from "../components/home.right.sidebar/home-right-sidebar-component";
+import { StatsComponent } from "../components/stats/stats.component";
+import { UserRankingComponent } from "../components/user.ranking/user.ranking.component";
+import { UserEntryComponent } from "../components/user.entry/user.entry.component";
 
 @NgModule({
   declarations: [
     RootContainer,
-    NavbarComponent
+    NavbarComponent,
+    HomeLeftSidebarComponent,
+    RecentPostComponent,
+    HomeRightSidebarComponent,
+    StatsComponent,
+    UserRankingComponent,
+    UserEntryComponent
   ],
   imports: [
     BrowserModule
